2a312606280143d59b80bd473e66eecadc6ea6d1
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2021-01-07 10:10:22 +0530

    created a new workshop script

---
+++ Scripts/Advanced R Markdown Workshop rconf2019.R
@@ -0,0 +1,34 @@
+# Pre workshop
+
+# Install required packages
+
+# arm_from_cran <- c("flexdashboard", "learnr", "bookdown",
+#                    "officer", "rticles", "webshot",
+#                    "tidyverse", "remotes", "babynames", "magick")
+# install.packages(arm_from_cran, dependencies = TRUE)
+# arm_from_gh <- c('yihui/xaringan', 'rstudio/blogdown',
+#                  'rstudio-education/armcompanion', 
+#                  'haozhu233/kableExtra', 'apreshill/bakeoff',
+#                  'hebrewseniorlife/memor')
+# remotes::install_github(arm_from_gh, dependencies = TRUE)
+# 
+# webshot::install_phantomjs()
+# 
+# 1
+# blogdown::install_hugo()
+# 
+# blogdown::hugo_version() # to check your version
+# blogdown::update_hugo() # to force an update
+# 
+# # A function to check if a package is installed on this system or not.
+# 
+# is_installed <- function(pkg_name){
+#     setNames(is.element(pkg_name, installed.packages()[, "Package"]), pkg_name)
+# } 
+# 
+# is_installed("xaringan")
+# 
+# is_installed(c(arm_from_cran, 'xaringan', 'blogdown', 'armcompanion', 'kableExtra', 'bakeoff'))
+# 
+
+# 
